1aa37c60a52c603d5ae7933e00ecb5ab2e15cdd5
Макушенко Матвей <matvey.myown@gmail.com>
2026-01-15 18:44:42 +0300

    It's have been started!

---
+++ src/js/get-arrivals.js
@@ -0,0 +1,11 @@
+const arrivalButton = document.getElementById("main-page-arrival-button")
+const arrivalsDepartures = document.getElementById("arrivals-departures")
+
+arrivalButton.addEventListener("click", function () {
+    console.log("ok")
+})
+
+
+// 1. Poluchit' dannie
+// 2. Render
+// 3. Prelodaer
